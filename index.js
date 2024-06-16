d4d2063b23d79826833ab1043eff2f223cc1c15f
MikesKids <michael.dantzler@gmail.com>
2024-06-15 20:08:00 -0400

    Create and Index function working

--- index.js
+++ index.js
@@ -1,23 +1,31 @@
 const { readJSONFile, writeJSONFile } = require("./src/helpers");
+const { create, index, show } = require("./src/carController");
 
 const inform = console.log;
 
 function run() {
   const action = process.argv[2];
   const car = process.argv[3];
+  const model = process.argv[4];
+  const cost = process.argv[5];
   let cars = readJSONFile("./data", "cars.json");
+  let writeToFile = false;
+  let updatedCars = [];
   switch (action) {
     case "index":
-      inform(action, cars);
+      const carsView = index(cars);
+      inform(carsView);
       break;
     case "create":
-      inform(action, car);
+      updatedCars = create(cars, car, model, cost);
+      writeToFile = true;
       break;
     case "show":
-      inform(action, car);
+      inform(cars, car);
       break;
     case "update":
-      inform(action, car);
+      const carShow = show(cars, car);
+      inform(carShow);
       break;
     case "destroy":
       inform(action, car);
@@ -25,5 +33,10 @@ function run() {
     default:
       inform("There was an error with this model");
   }
+  if (writeToFile) {
+    writeJSONFile("./data", "cars.json", updatedCars);
+  }
 }
 run();
+
+module.exports = { run };
--- src/carController.js
+++ src/carController.js
@@ -1,5 +1,35 @@
-function create(cars, carName) {
-  const car = { name: carName };
+const { nanoid } = require("nanoid");
+const { readJSONFile, writeJSONFile } = require("./helpers");
+
+function create(cars, carName, modelName, priceInCents) {
+  const USDollar = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+  });
+  const car = {
+    name: carName,
+    model: modelName,
+    price: `${USDollar.format(priceInCents)}`,
+    id: nanoid(4),
+    inStock: true,
+  };
   cars.push(car);
   return cars;
 }
+
+function index(cars) {
+  return cars.map((car) => car.name + " " + car.model).join("\n");
+}
+
+function show(cars, carId) {
+  const USDollar = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+  });
+  const car = cars.find((car) => car.id === carId);
+  return `Brand: ${car.name} \n Model: ${car.model} \n Price: ${USDollar.format(
+    car.price
+  )} \n In Stock: ${car.inStock}`;
+}
+
+module.exports = { create, index, show };
